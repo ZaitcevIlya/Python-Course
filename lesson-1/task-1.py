17f8ddb533bd6037e06c75fec3faa99fbadd5def
ZaitcevIlya <izaytsev.work@gmail.com>
2021-01-31 17:07:25 +0300

    completed homework for lesson 1

---
+++ lesson-1/task-1.py
@@ -0,0 +1,14 @@
+# 1. Поработайте с переменными, создайте несколько, выведите на экран,
+# запросите у пользователя несколько чисел и строк и сохраните в переменные,
+# выведите на экран.
+
+name = 'Ilya'
+age = 30
+
+print(f'My name is {name}, I`m {age} years old')
+
+user_name = input('What is your name?')
+user_age = input('How old are you?')
+color = input('What is your favorite color?')
+
+print(f'You are {user_name} and your age is {user_age}. And you are obsessed by {color}. Just a joke.')
